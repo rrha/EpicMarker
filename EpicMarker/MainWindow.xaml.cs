b3fd15c19ffeb712a457857c7f08a86406399d4d
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-10-06 19:47:53 +0100

    :+1:

---
+++ EpicMarker/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EpicMarker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Tools toolsWindow = new Tools();
+        public MainWindow()
+        {
+            InitializeComponent();
+            toolsWindow.Topmost = true;
+            toolsWindow.Show();
+        }
+        private void inkCanvas1_Gesture(object sender, InkCanvasGestureEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ EpicMarker/Tools.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EpicMarker
+{
+    /// <summary>
+    /// Interaction logic for Tools.xaml
+    /// </summary>
+    public partial class Tools : Window
+    {
+        public Tools()
+        {
+            InitializeComponent();
+        }
+        private void Move(object sender, RoutedEventArgs e)
+        {
+            this.DragMove();
+            e.Handled = true;
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.Strokes.Clear();
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.Close();
+        }
+    }
+}
