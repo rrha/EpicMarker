be8a121be06d4a9d1d1d8715478c83f308ab4e77
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-10-07 19:21:30 +0100

    :+1:

--- EpicMarker/Tools.xaml.cs
+++ EpicMarker/Tools.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,10 +9,12 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+
 
 namespace EpicMarker
 {
@@ -31,123 +35,143 @@ namespace EpicMarker
         }
         private void button_1_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
             mainWindow.Hide();
         }
         private void button0_Click(object sender, RoutedEventArgs e)
         {
 
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
             mainWindow.Show();
 
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
             mainWindow.inkCanvas1.Strokes.Clear();
         }
         private void button2_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
             mainWindow.Close();
         }
         private void button3_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#00FF18");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#00FF18");
 
         }
         private void button4_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#00f0ff");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#00f0ff");
 
         }
         private void button5_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#ff0000");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#ff0000");
 
         }
         private void button6_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#f55096");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#f55096");
 
         }
         private void button7_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#fff36a");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#fff36a");
 
         }
         private void button8_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#ff7800");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#ff7800");
 
         }
         private void button9_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#402b56");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#402b56");
 
         }
         private void button10_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#595b5c");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#595b5c");
 
         }
         private void button11_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#ffffff");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#ffffff");
 
         }
         private void button12_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#bdc3c7");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#bdc3c7");
 
         }
         private void button13_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#000000");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#000000");
 
         }
         private void button14_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (Color)ColorConverter.ConvertFromString("#502d1d");
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            mainWindow.inkCanvas1.DefaultDrawingAttributes.Color = (System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#502d1d");
 
         }
         private void button20_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
             mainWindow.inkCanvas1.DefaultDrawingAttributes.Width = 5f;
             mainWindow.inkCanvas1.DefaultDrawingAttributes.Height = 5f;
         }
         private void button21_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
             mainWindow.inkCanvas1.DefaultDrawingAttributes.Width = 8f;
             mainWindow.inkCanvas1.DefaultDrawingAttributes.Height = 8f;
         }
         private void button22_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
             mainWindow.inkCanvas1.DefaultDrawingAttributes.Width = 10f;
             mainWindow.inkCanvas1.DefaultDrawingAttributes.Height = 10f;
         }
         private void button23_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            MainWindow mainWindow = System.Windows.Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
             mainWindow.inkCanvas1.DefaultDrawingAttributes.Width = 13f;
             mainWindow.inkCanvas1.DefaultDrawingAttributes.Height = 13f;
+        }
+        private void button24_Click(object sender, RoutedEventArgs e)
+        {
+            int screenLeft = SystemInformation.VirtualScreen.Left;
+            int screenTop = SystemInformation.VirtualScreen.Top;
+            int screenWidth = SystemInformation.VirtualScreen.Width;
+            int screenHeight = SystemInformation.VirtualScreen.Height;
+
+            using (Bitmap bmp = new Bitmap(screenWidth, screenHeight))
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    String path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+                    DateTime.Now.ToString("ddMMyyyy-hhmmss") + ".png");
+                    Opacity = .0;
+                    g.CopyFromScreen(screenLeft, screenTop, 0, 0, bmp.Size);
+                    bmp.Save(path);
+                    Opacity = 1;
+                }
+            }
         }
     }
 }
