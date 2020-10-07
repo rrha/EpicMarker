be6712142c7644f0a6d8b2bcec30f922efff3af5
SaidRH <36697955+SaidRH@users.noreply.github.com>
2020-10-08 00:31:02 +0100

    :+1:

--- EpicMarker/Properties/AssemblyInfo.cs
+++ EpicMarker/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EpicMarker")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©era7im  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
